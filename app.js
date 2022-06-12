77586b16379cacadd395c71c9f11cd0fe5f245bf
maxypunzo <maxwellpunzo@gmail.com>
2022-06-12 21:30:50 +0200

    Added mad-lib replacement variables to story

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 "use strict"
+let userInputs = [1980, "Tom Ben Lee", "Error"];
+for (let x = 0; x < 3; x++) {
+    let originalStory = `In  >${userInputs[0]}, computer pioneer  >${userInputs[1]} found herself working on a  >${userInputs[2]}. 
+    Word got out that the team had "debugged" the  >${userInputs[2]}`;
+    console.log(originalStory);
+}
+
+// let originalStory = `In 1980, computer pioneer Tom Ben Lee found herself working on a Error. 
+// Word got out that the team had "debugged" the Issue`;
+// ${userInputs[x]}
+
