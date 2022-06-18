c0013cc6dc8aac054ac46cfb0b3626be5954ff2e
maxypunzo <maxwellpunzo@gmail.com>
2022-06-18 19:07:26 +0200

    added user prompt

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-"use strict"
-let userInputs = [1980, "Tom Ben Lee", "Error"];
+// "use strict"
+
+(function() {
+    let userInputs = [];
+    for (var x = 0; x < 10; x++) {
+        userInputs[x] = prompt('Enter your name');
+    }
+    console.log(userInputs);
+})();
+
 for (let x = 0; x < 3; x++) {
     let originalStory = `In  >${userInputs[0]}, computer pioneer  >${userInputs[1]} found herself working on a  >${userInputs[2]}. 
     Word got out that the team had "debugged" the  >${userInputs[2]}`;
